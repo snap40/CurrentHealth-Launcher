0b8142038977c6b292225d3c9d09406e2c90cab1
ishamch <123366066+ishamch@users.noreply.github.com>
2024-04-01 10:28:42 +0100

    Added FB Crashlytics, remote config, configurable delay and toast messages

--- RaptorHealthMonitor/app/build.gradle.kts
+++ RaptorHealthMonitor/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -11,8 +13,8 @@ android {
         applicationId = "com.currenthealth.raptorhealthmonitor"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -41,6 +43,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-config:21.6.3")
+    implementation("com.google.firebase:firebase-crashlytics:18.6.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- RaptorHealthMonitor/app/src/main/java/com/currenthealth/raptorhealthmonitor/MainActivity.kt
+++ RaptorHealthMonitor/app/src/main/java/com/currenthealth/raptorhealthmonitor/MainActivity.kt
@@ -1,27 +1,40 @@
 package com.currenthealth.raptorhealthmonitor
 
-import android.content.ActivityNotFoundException
-import android.content.ComponentName
+import android.app.ActivityManager
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.content.Intent.CATEGORY_BROWSABLE
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.content.Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.currenthealth.raptorhealthmonitor.util.TAG
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var appLaunchDelay = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpFireBaseRemoteConfig()
+        launchRaptorApp()
     }
 
     override fun onResume() {
@@ -32,17 +45,58 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
+    private fun setUpFireBaseRemoteConfig() {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
 
-    private fun launchApps(){
-        launchVivaLinkApp()
-        runBlocking {
-            Log.i(TAG(), "onCreate: runBlobking")
-            launch {
-                Log.i(TAG(), "onCreate: launch")
-                delay(5000)
-                launchRaptorApp()
+        remoteConfig.fetchAndActivate().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                runOnUiThread {
+                    showToast("Remote config fetched successfully!")
+                }
             }
         }
+
+        remoteConfig.addOnConfigUpdateListener(object : ConfigUpdateListener {
+            override fun onUpdate(configUpdate: ConfigUpdate) {
+                Log.d(TAG(), "Updated keys: " + configUpdate.updatedKeys);
+
+                if (configUpdate.updatedKeys.contains("appLaunchDelay")) {
+                    remoteConfig.activate().addOnCompleteListener {
+                        appLaunchDelay = remoteConfig.getLong("appLaunchDelay")
+                        showToast("App Launch delay updated to $appLaunchDelay")
+                    }
+                }
+            }
+
+            override fun onError(error: FirebaseRemoteConfigException) {
+                Log.w(TAG(), "Config update error with code: " + error.code, error)
+            }
+        })
+
+    }
+
+    private fun launchApps() {
+        appLaunchDelay = Firebase.remoteConfig.getLong("appLaunchDelay")
+        showToast("App Launch delay is $appLaunchDelay")
+        launchVivaLinkApp()
+        Thread(){ //Effort to make it smooth for the PFA app to launch -> Too much for the hybrid app maybe?
+            //Observed PFA showing black screen and crash
+            runBlocking (Dispatchers.IO) {
+                Log.i(TAG(), "onCreate: runBlobking")
+                delay(appLaunchDelay)
+                launch(Dispatchers.Main) {
+                    Log.i(TAG(), "onCreate: launch")
+//                    delay(appLaunchDelay)
+                    launchRaptorApp()
+                }
+            }
+        }.start()
+
     }
 
 
@@ -52,18 +106,27 @@ class MainActivity : AppCompatActivity() {
             flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_REQUIRE_NON_BROWSER
         }
         startActivity(raptorIntent)
+        showToast("MVM Launched")
         Log.i(TAG(), "launchVivaLinkApp: COMPLETED")
     }
 
     private fun launchRaptorApp() {
         val raptorIntent =
             Intent(Intent.ACTION_VIEW, Uri.parse("com.currenthealth.velocigoose://")).apply {
-
                 addCategory(CATEGORY_BROWSABLE)
                 flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_REQUIRE_NON_BROWSER
             }
 
         startActivity(raptorIntent)
+        showToast("PFA Launched")
         Log.i(TAG(), "launchRaptorApp: COMPLETED")
     }
+
+    private fun showToast(message: String) {
+        val showToastStatus = Firebase.remoteConfig.getBoolean("showToastMessages")
+        if (showToastStatus) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
